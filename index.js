6be95e56d77f27611605f637f2f7bea6aa5030c8
Héliton Martins <helitonmrf@gmail.com>
2020-05-29 20:34:00 -0300

    Busca no Google Images implementada.

--- index.js
+++ index.js
@@ -9,27 +9,24 @@ class Main {
         this.text = new Text();
     }
     async start(){
-        //TODO: molécula não é escolhida
-        // const molecule = chooseMolecule();
-        const molecule = "alanine";
+        const molecule = chooseMolecule();
+        // const molecule = "Pepstatin";
         const wikiPage = await this.wiki.getWikiIntroText(molecule);
         const properties = await this.wiki.getProperties();
-        //TODO: cancelei a imagem para testar a geração via smiles.
-        properties.image = "";
         const image = await getStructure(properties.image, properties.smiles, molecule);
         const tweet = await this.text.prepareSummary(wikiPage.extract, Wiki.getPageLinkByTitle(molecule), molecule);
         console.log("molecule: ", molecule)
-        // console.log("tweet: ", tweet)
+        console.log("tweet: ", tweet)
         console.log("\nimage:", image)
-        console.log("smiles", properties.smiles)
+        // console.log("smiles", properties.smiles)
         // console.log("\nproperties:", properties)
-    }
-
-    // TODO mover isso para src/getStructure.js.
-    getImage(molecule, wiki_image_url){
-        return wiki_image_url || undefined
     }
 }
 
 const mainInstance = new Main();
 mainInstance.start();
+// const mI = {}
+// for(let i = 0; i < 20; i++){
+//     mI[i] = new Main();
+//     mI[i].start();
+// }
