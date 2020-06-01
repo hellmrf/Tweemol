286d833785da70b45daab471ddbcabab2493e1b6
Héliton Martins <helitonmrf@gmail.com>
2020-06-01 16:22:36 -0300

    Mudando o diretório de download das imagens para /public

--- src/images.js
+++ src/images.js
@@ -8,7 +8,7 @@ const sharp = require('sharp');
 
 const googleSearchCredentials = require('../credentials/google-search.json');
 
-const IMAGES_PATH = path.resolve(__dirname, '../temp_images');
+const IMAGES_PATH = path.resolve(__dirname, '../public/temp_images');
 
 /*
     Path -> local address to image file
